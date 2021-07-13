67a58d94d36d61f560dc4c96385b5a3583129936
Kalcifer <7829231+K4LCIFER@users.noreply.github.com>
2021-07-12 21:44:13 -0700

    changed the program formatting to use 4 spaces in place of tabs.

--- gnome-app_grid-cleaner.py
+++ gnome-app_grid-cleaner.py
@@ -2,21 +2,21 @@ import os
 
 application_directory = '/usr/share/applications'
 
-whitelist = {}
+whitelist = {}  # This is 
 
 blacklist = {}
 
-applications_to_remove = os.listdir(application_directory) # Load all the applications into a list. The ones that are to not be removed will be removed from this list in the following lines.
+applications_to_remove = os.listdir(application_directory)  # Load all the applications into a list. The ones that are to not be removed will be removed from this list in the following lines.
 
 for application in os.listdir(application_directory):
-	if (application in whitelist) or ('.desktop' not in application): # If the file is in the whitlisted applications to keep, or if the file is not an application, then add the file to the remove list.
-		applications_to_remove.remove(application)
-	with open(application_directory + '/' + application, 'r') as application_file:
-		for line in application_file:
-			if 'NoDisplay=true' in line: # Check if the file has already been removed from the application list.
-				applications_to_remove.remove(application)
+    if (application in whitelist) or ('.desktop' not in application):   # If the file is in the whitlisted applications to keep, or if the file is not an application, then add the file to the remove list.
+        applications_to_remove.remove(application)
+    with open(application_directory + '/' + application, 'r') as application_file:
+        for line in application_file:
+            if 'NoDisplay=true' in line:    # Check if the file has already been removed from the application list.
+                applications_to_remove.remove(application)
 
 for application in applications_to_remove:
-	print(application)
-	with open(application_directory + '/' + application, 'a') as application_file:
-		application_file.write('NoDisplay=true') # Hide the application from the application menu
+    print(application)
+    with open(application_directory + '/' + application, 'a') as application_file:
+        application_file.write('NoDisplay=true')    # Hide the application from the application menu
