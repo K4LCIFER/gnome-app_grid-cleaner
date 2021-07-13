6a9ba37d5ff68eb5db3f3b163f1d3bcfa1ea63cb
Kalcifer <7829231+K4LCIFER@users.noreply.github.com>
2021-07-12 22:00:12 -0700

    Added some documentation to the python code.

--- gnome-app_grid-cleaner.py
+++ gnome-app_grid-cleaner.py
@@ -1,14 +1,14 @@
 import os
 
-application_directory = '/usr/share/applications'
+application_directory = '/usr/share/applications'   # This is the directory where the applicaton `.desktop` files are kept.
 
-whitelist = {}  # This is 
+whitelist = {}  # Applications that are to be kept, are added to this list as a string.
 
-blacklist = {}
+#blacklist = {} # Not sure if this will ever be used. 
 
 applications_to_remove = os.listdir(application_directory)  # Load all the applications into a list. The ones that are to not be removed will be removed from this list in the following lines.
 
-for application in os.listdir(application_directory):
+for application in os.listdir(application_directory):   # Loop through all files in the application directory.
     if (application in whitelist) or ('.desktop' not in application):   # If the file is in the whitlisted applications to keep, or if the file is not an application, then add the file to the remove list.
         applications_to_remove.remove(application)
     with open(application_directory + '/' + application, 'r') as application_file:
