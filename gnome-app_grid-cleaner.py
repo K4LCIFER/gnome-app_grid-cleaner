82b1f131e55f088f09f1292fa5c11dfa9ffe0481
Kalcifer <7829231+K4LCIFER@users.noreply.github.com>
2021-08-05 00:38:44 -0700

    added and documented the --remove command.

--- gnome-app_grid-cleaner.py
+++ gnome-app_grid-cleaner.py
@@ -1,3 +1,4 @@
+
 import os
 import argparse
 
@@ -19,16 +20,14 @@ parser_clean = subparsers.add_parser('clean')
 
 args = parser.parse_args()
 
-print(args)
-
-if args.subcommand == 'whitelist':
-    if args.add:
+if args.subcommand == 'whitelist':  # For modifying the whitelist.
+    if args.add:    # For adding entries to the whitelist.
         with open('whitelist', 'r+') as whitelist:
         # 'r+' is for read and write, but the file pointer is placed at the
         # beginning of the file. The following `read()` command will then put
         # the file pointer at the end of the file.
-            whitelist_content = whitelist.read()    # Store whitelist contents
-            # Loop thuough all of the files that were specified in the command
+            whitelist_content = whitelist.read()    # Store whitelist contents.
+            # Loop thuough all of the files that were specified in the command.
             for application_filename in args.add:
                 # If the application filename is already within the whitelist, 
                 # then don't add it.
@@ -39,8 +38,28 @@ if args.subcommand == 'whitelist':
                 elif application_filename not in whitelist_content: 
                     print("Added " + application_filename)
                     whitelist.write(application_filename + '\n')
-    elif args.remove:
-        pass
+    elif args.remove:   # For removing entries from the whitelist.
+        with open('whitelist', 'r') as original_whitelist:
+            original_whitelist_contents = original_whitelist.read()
+            original_whitelist.seek(0)  # Reset the file-pointer after reading.
+            # Check if the requested application filename to be removed is
+            # already absent from the whitelist. If so, raise a warning.
+            for application_filename in args.remove:
+                if application_filename not in original_whitelist_contents:
+                    print(application_filename + " is not whitelisted.")
+            with open('new_whitelist', 'w') as new_whitelist:
+                # Go through the whitelist's contents, and only keep that which
+                # has not been requested to be removed.
+                for line in original_whitelist:
+                    # Must strip the newline, `\n` character from the end of
+                    # each line so that a match can be made.
+                    if line.strip("\n") not in args.remove:
+                        new_whitelist.write(line)
+                    elif line.strip("\n") in args.remove:
+                        print('Removed ' + line)
+        # Update the whitelist file.
+        os.remove('whitelist')
+        os.rename('new_whitelist', 'whitelist')
     elif args.list:
         pass
     elif args.discriminate:
